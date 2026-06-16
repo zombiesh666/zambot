e3f7adc16c96cfadc435a3713d79becbbf2451a9
Dmytro Yushkin <zombiesh@gmail.com>
2026-06-16 11:35:26 -0500

    fix: reduce event type, g code is gone

--- src/sync_manager.py
+++ src/sync_manager.py
@@ -62,7 +62,7 @@ class SyncManager:
                     "filter[start__lte]": end_str, "include": "eventType,summary,resource.facility"
                 }
 
-                target_codes = ["10", "12", "15", "16", "17", "22", "g"]
+                target_codes = ["10", "12", "15", "16", "17", "22"]
                 for index, code in enumerate(target_codes):
                     base_params[f"filter[or][{index}][eventType.code]"] = code
 
@@ -128,7 +128,7 @@ class SyncManager:
                     "filter[start__lte]": end_str, "include": "eventType,summary,resource.facility"
                 }
 
-                chap_codes = ["6", "9", "12", "13", "g"]
+                chap_codes = ["6", "9", "12", "13"]
                 for index, code in enumerate(chap_codes):
                     base_params[f"filter[or][{index}][eventType.code]"] = code
 
